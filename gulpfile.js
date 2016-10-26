8bfdafd304dee37b345dc3fe9acbfe9f646ba092
sashakozakov <sashakozakov2293@gmail.com>
2016-10-26 23:47:47 +0300

    some fixes

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	browserSync = require('browser-sync'),
 	autoprefixer = require('gulp-autoprefixer'),
-	sourcemaps = require('gulp-sourcemaps');
+	sourcemaps = require('gulp-sourcemaps'),
+  iconfont = require('gulp-iconfont'),
+  runTimestamp = Math.round(Date.now()/1000);
 
 
 gulp.task('sass', function(){
@@ -39,3 +41,17 @@ gulp.task('browserSync', function() {
   })
 })
 
+gulp.task('Iconfont', function(){
+  return gulp.src(['app/images/*.svg'])
+    .pipe(iconfont({
+      fontName: 'myfont', // required
+      prependUnicode: true, // recommended option
+      formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
+      timestamp: runTimestamp, // recommended to get consistent builds when watching files
+    }))
+      .on('glyphs', function(glyphs, options) {
+        // CSS templating, e.g.
+        console.log(glyphs, options);
+      })
+    .pipe(gulp.dest('app/fonts/'));
+});
