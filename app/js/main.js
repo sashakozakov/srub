b9b4a8844eb76e5dd5283b1c1c9e9a79c3544d15
sashakozakov <sashakozakov2293@gmail.com>
2016-10-29 11:31:45 +0300

    added some blocks

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,15 @@
 	$(document).ready(function () {
 
 
+		$('a[href*=#]').bind("click", function (e) {
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+
 		//Make elements equal height
 		$('.matchHeight').matchHeight();
 
